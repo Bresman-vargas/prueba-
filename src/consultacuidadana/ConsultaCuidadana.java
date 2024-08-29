800f5e17443fac649ae6657d341f1983e8b8cb45
Bresman-vargas <163463362+Bresman-vargas@users.noreply.github.com>
2024-08-29 00:08:33 -0400

    Registro de Beneficiarios

---
+++ src/consultacuidadana/ConsultaCuidadana.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package consultacuidadana;
+
+/**
+ *
+ * @author bresm
+ */
+public class ConsultaCuidadana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hola mundo");
+    }
+    
+}
